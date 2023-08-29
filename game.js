329d0ad86bb870f5dc71ec1a232e9fa6b96c9287
Rishaan111k <139289393+Rishaan111k@users.noreply.github.com>
2023-08-29 19:14:11 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,56 @@
+function Send(){
+    number1 = document.getElementById("number1").value
+    number2 = document.getElementById("number2").value
+    actual_answer = parseInt(number1) * parseInt(number2);
+}
+question = "<h4>"+ number1 + "*" + number2 +"</h4>"
+input = "<br> Answer : <input type = 'text' id='input_check_box'>";
+check = "<br><br><button class='btn btn-warning' onclick='check()'>CHECK</button>";
+row= question+input+check;
+
+document.getElementById("output").innerHTML = row;
+
+document.getElementById("number1").value="";
+document.getElementById("number2").value="";
+
+question_turn="player1";
+answer_turn="player2";
+
+function check(){
+    get_answer = documnet.getElementById("input_check_box").value;
+    if(get_answer == actual_answer){
+        if(answer_turn == "player1"){
+            update_player1_score = player1_score+1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+
+        }
+        else{
+            update_player2_score = player2_score+1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+
+        }
+
+    }
+    if(question_turn == "player1")
+    {
+        question_turn = "player2"
+        document.getElementById("player_question").innerHTML = "Question Turn -" +player2_name;
+    }
+    else
+    {
+        question_turn="player1"
+        document.getElementById("player_question").innerHTML = "Question Turn -" +player2_name;
+    }
+
+    if(question_turn == "player1")
+    {
+        question_turn = "player2"
+        document.getElementById("player_answer").innerHTML = "Answer Turn -" +player2_name;
+    }
+    else
+    {
+        question_turn="player1"
+        document.getElementById("player_answer").innerHTML = "Answer Turn -" +player1_name;
+    }
+}
+
